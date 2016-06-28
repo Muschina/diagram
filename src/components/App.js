3956bbef941637fe75a9d546111a116526ac8396
Muschina <mus_china@mail.ru>
2016-06-28 17:22:07 +0300

    init dev enviroment

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './app.less';
+
+export default class App extends Component {
+  render() {
+    return <div className='container'>diagram</div>
+  }
+}
